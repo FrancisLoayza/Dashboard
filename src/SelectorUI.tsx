9b5f77ffdc4bdb659cc046bdae755ee9fc148911
FrancisLoayza <faloayza@espol.edu.ec>
2026-07-08 14:16:50 -0500

    Update SelectorUI.tsx

--- src/SelectorUI.tsx
+++ src/SelectorUI.tsx
@@ -7,6 +7,7 @@ import { useState } from 'react';
 
 export default function SelectorUI() {
     const [cityInput, setCityInput] = useState('');
+    
     const handleChange = (event: SelectChangeEvent<string>) => {
         setCityInput(event.target.value)
         alert(event.target.value)
@@ -20,18 +21,18 @@ return (
          label="Ciudad"
          onChange={handleChange}
          value={cityInput}>
-            {cityInput && (
-            <p>
-                Información del clima en <span style={{textTransform: 'capitalize', fontWeight: 'bold'}}>{cityInput}</span>
-            </p>
-        )}
          <MenuItem disabled><em>Seleccione una ciudad</em></MenuItem>
          <MenuItem value={"guayaquil"}>Guayaquil</MenuItem>
          <MenuItem value={"quito"}>Quito</MenuItem>
          <MenuItem value={"manta"}>Manta</MenuItem>
          <MenuItem value={"cuenca"}>Cuenca</MenuItem>
       </Select>
-
+{cityInput && (
+            <p>
+                Información del clima en <span style={{textTransform: 'capitalize', fontWeight: 'bold'}}>{cityInput}</span>
+            </p>
+        )}
    </FormControl>
-   )
+
+)
 }
